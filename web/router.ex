3577b0030054137c134cc7c2826654a195019b27
Ahmed Hawas <ahmed.hawas@thinkresearchgroup.com>
2016-12-31 18:55:09 -0500

    create cards migration

--- web/router.ex
+++ web/router.ex
@@ -1,26 +1,13 @@
 defmodule PhxOembed.Router do
   use PhxOembed.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/", PhxOembed do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :api
 
-    get "/", PageController, :index
+    get "/", CardController, :show
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", PhxOembed do
-  #   pipe_through :api
-  # end
 end
